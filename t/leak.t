TBSliver/Web-Simple

commit 1f8cad5e5a1875de94d63ac91d8ded4d2282c62e

disassemble DispatchNode since Dispatch.pm can now differentiate between dispatch app and object

lib/Web/Dispatch.pm
@@ -21,7 +21,6 @@ has parser_class => (
 has node_class => (
   is => 'ro', default => quote_sub q{ 'Web::Dispatch::Node' }
 );
-has node_args => (is => 'ro', default => quote_sub q{ {} });
 has _parser => (is => 'lazy');
 
 after BUILDARGS => sub {
@@ -144,7 +143,12 @@ sub _to_try {
 sub _construct_node {
   my ($self, %args) = @_;
   $args{match} = $self->_parser->parse($args{match}) if !ref $args{match};
-  $self->node_class->new({ %{$self->node_args}, %args })->to_app;
+  if ( my $obj = $self->dispatch_object) {
+    # if possible, call dispatchers as methods of the app object
+    my $dispatch_sub = $args{run};
+    $args{run} = sub { $obj->$dispatch_sub(@_) };
+  }
+  $self->node_class->new(\%args)->to_app;
 }
 
 1;

t/dispatch_misc.t
@@ -23,6 +23,7 @@ sub run_request { $app->run_test_request( @_ ); }
 
 app_is_non_plack();
 app_is_object();
+app_is_just_sub();
 plack_app_return();
 broken_route_def();
 invalid_psgi_responses();
@@ -57,6 +58,14 @@ sub app_is_object {
     my $res = $d->call;
 
     cmp_ok $res->[0], '==', 999, "Web::Dispatch can dispatch properly, given only an object with to_app method";
+}
+
+sub app_is_just_sub {
+    my $d = Web::Dispatch->new( dispatch_app => sub () { [ 999, [], ["ok"] ] } );
+    my $res = $d->call( {} );
+
+    cmp_ok $res->[0], '==', 999,
+      "Web::Dispatch can dispatch properly, given only an app that's just a sub, with no object involved";
 }
 
 sub plack_app_return {

lib/Web/Dispatch/Node.pm
@@ -20,7 +20,12 @@ sub call {
 sub _curry {
   my ($self, @args) = @_;
   my $run = $self->_run;
-  sub { $run->(@args, $_[0]) };
+  my $code = sub { $run->(@args, $_[0]) };
+  # if the first argument is a hashref, localize %_ to it to permit
+  # use of $_{name} inside the dispatch sub
+  ref($args[0]) eq 'HASH'
+    ? do { my $v = $args[0]; sub { local *_ = $v; &$code } }
+    : $code
 }
 
 1;

lib/Web/Simple/Application.pm
@@ -37,13 +37,12 @@ sub _build__dispatcher {
   # closes back over $self
 
   weaken($self);
-  my $node_args = { app_object => $self };
-  weaken($node_args->{app_object});
-  Web::Dispatch->new(
+  my %dispatch_args = (
     dispatch_app => sub { $self->dispatch_request(@_), $final },
-    node_class => 'Web::Simple::DispatchNode',
-    node_args => $node_args
+    dispatch_object => $self
   );
+  weaken($dispatch_args{dispatch_object});
+  Web::Dispatch->new(%dispatch_args);
 }
 
 sub _build_final_dispatcher {

lib/Web/Simple/DispatchNode.pm
@@ -1,20 +1,0 @@
-package Web::Simple::DispatchNode;
-
-use Moo;
-
-extends 'Web::Dispatch::Node';
-
-has _app_object => (is => 'ro', init_arg => 'app_object', required => 1);
-
-around _curry => sub {
-  my ($orig, $self) = (shift, shift);
-  # this ensures that the dispatchers get called as methods of the app itself
-  my $code = $self->$orig($self->_app_object, @_);
-  # if the first argument is a hashref, localize %_ to it to permit
-  # use of $_{name} inside the dispatch sub
-  ref($_[0]) eq 'HASH'
-    ? do { my $v = $_[0]; sub { local *_ = $v; &$code } }
-    : $code
-};
-
-1;

commit 1f727762b7d2af3c22ae86a9c9012154a82b1457

fix a memory leak introduced by 1f8cad5e5a1875de94d63ac91d8ded4d2282c62e

lib/Web/Dispatch.pm
@@ -14,7 +14,7 @@ with 'Web::Dispatch::ToApp';
 has dispatch_app => (
   is => 'lazy', builder => sub { shift->dispatch_object->to_app }
 );
-has dispatch_object => (is => 'ro', required => 0);
+has dispatch_object => (is => 'ro', required => 0, weak_ref => 1);
 has parser_class => (
   is => 'ro', default => quote_sub q{ 'Web::Dispatch::Parser' }
 );

t/dispatch_misc.t
@@ -64,7 +64,8 @@ sub app_is_object {
         sub to_app { [ 999, [], ["ok"] ] }
     }
 
-    my $d = Web::Dispatch->new( dispatch_object => ObjectApp->new );
+    my $o = ObjectApp->new;
+    my $d = Web::Dispatch->new( dispatch_object => $o );
     my $res = $d->call;
 
     cmp_ok $res->[0], '==', 999, "Web::Dispatch can dispatch properly, given only an object with to_app method";

t/leak.t
@@ -1,0 +1,15 @@
+use strictures;
+use Test::More eval { require Devel::Cycle } ? 'no_plan' : ( skip_all => 'No Devel::Cycle' );
+
+use Web::Simple;
+use Devel::Cycle;
+
+my $counter;
+my $on_cycle = sub { Devel::Cycle::_do_report( ++$counter, shift ) };
+{
+    local *STDOUT = *STDERR;
+    Devel::Cycle::find_cycle( main->new->to_psgi_app, $on_cycle );
+}
+ok !$counter, "no leak in to_psgi_app";
+
+done_testing;